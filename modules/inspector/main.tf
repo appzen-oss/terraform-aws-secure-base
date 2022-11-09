4a63b554ac030a2f88b9ef7d64214a79cdc92f91
wesleung <wes@appzen.com>
2022-11-09 11:50:53 -0800

    Inspector2 delegate

--- modules/inspector/main.tf
+++ modules/inspector/main.tf
@@ -10,17 +10,7 @@
 #aws inspector2 enable-delegated-admin-account --delegated-admin-account-id ID
 #aws inspector2 update-organization-configuration --auto-enable ec2=false,ecr=true
 
-# tf does not support inspector2 yet
-
-#  unrecognized service principal
-# Organization Master - Regional
-#resource "aws_organizations_delegated_administrator" "inspector2" {
-#  count             = var.enable && var.account_type == "master" ? 1 : 0
-#  account_id        = var.org_administrator_account_id
-#  service_principal = "inspector2.amazonaws.com"
-#}
-
 resource "aws_inspector2_delegated_admin_account" "self" {
-  count      = var.enable ? 1 : 0
+  count      = var.enable && var.account_type == "master" ? 1 : 0
   account_id = var.security_administrator_account_id
 }
