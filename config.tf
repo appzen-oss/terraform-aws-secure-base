c21ab7ee8fccf6a7b3965e11cc0aac16523f270a
wesleung <wes@appzen.com>
2022-12-09 13:02:35 -0800

    primary_region default to us-east-1

--- config.tf
+++ config.tf
@@ -4,6 +4,7 @@ module "config-us-east-1" {
   depends_on  = [aws_organizations_organization.self]
   enable      = var.enable_config && contains(var.target_regions, "us-east-1")
   account_type                      = var.account_type
+  primary_region                    = "us-east-1"
   s3_bucket_name                    = "appzen-log-infra-us-east-1"
   security_administrator_account_id = var.security_administrator_account_id
 }
@@ -14,6 +15,7 @@ module "config-us-east-2" {
   depends_on  = [aws_organizations_organization.self]
   enable      = var.enable_config && contains(var.target_regions, "us-east-2")
   account_type                      = var.account_type
+  primary_region                    = "us-east-2"
   s3_bucket_name                    = "appzen-log-infra-us-east-2"
   security_administrator_account_id = var.security_administrator_account_id
 }
@@ -24,6 +26,7 @@ module "config-eu-west-1" {
   depends_on  = [aws_organizations_organization.self]
   enable      = var.enable_config && contains(var.target_regions, "eu-west-1")
   account_type                      = var.account_type
+  primary_region                    = "eu-west-1"
   s3_bucket_name                    = "appzen-log-infra-eu-west-1"
   security_administrator_account_id = var.security_administrator_account_id
 }
